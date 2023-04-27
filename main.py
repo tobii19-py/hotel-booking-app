be457e50df10a0d1b88ab1c84fd075cc2daa8723
tobi <tobibags19@gmail.com>
2023-04-27 06:39:39 -0500

    Imported credit card dataframe and implemented the credit card class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -38,17 +39,28 @@ class ReservationTicket:
 
 
 class CreditCard:
-    pass
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, exp, holder, cvc):
+        card_data = {"number": self.number, "exp": exp,
+                     "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+
 
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = CreditCard(number="1234567890123456", exp="12/27", holder="JOHN SMITH", cvc="123")
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = CreditCard(number="1234567890123456")
+    if credit_card.validate(exp="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+        print(reservation_ticket.generate())
+    else:
+        print("There was a problem with your payment")
 else:
     print("Hotel is not available")
